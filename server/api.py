9e943871630da27c4c025532f6d908ff2fc9614b
alpha-beta-soup <richard.m.law@gmail.com>
2017-05-29 00:03:32 +1200

    refactoring

--- server/api.py
+++ server/api.py
@@ -9,6 +9,23 @@ from graphene.types.datetime import DateTime
 from search import (Point, GeoHash, query, NORMALISATION)
 
 
+def es_geo_index_result(hits, allow_multiple):
+    if not hits:
+        return False
+    return hits[slice(None)] if allow_multiple else hits[0]
+
+
+def get_es_props(esindex, props):
+    if isinstance(esindex, list):
+        return [get_es_props(idx, props) for idx in esindex]
+    else:
+        return {
+            prop: esindex.properties[NORMALISATION[esindex.meta.index][prop]]
+            if esindex else None
+            for prop in props
+        }
+
+
 class LocationInput(graphene.InputObjectType):
     lat = graphene.Float()
     lon = graphene.Float()
@@ -24,6 +41,8 @@ class CreateLocation(graphene.Mutation):
     dhb = graphene.Field(lambda: DHB)
     communityBoard = graphene.Field(lambda: CommunityBoard)
     schoolZone = graphene.List(lambda: SchoolZone)
+    postCode = graphene.Field(lambda: PostCode)
+    electorate2014 = graphene.Field(lambda: Electorate2014)
 
     @staticmethod
     def mutate(root, args, context, info):
@@ -35,59 +54,35 @@ class CreateLocation(graphene.Mutation):
         else:
             location = Point(lon, lat)
 
-        # TODO use the location to query ES here
         # << BUSINESS LOGIC >>
         get_hits_from_index = lambda hit, index: hit.meta.index == index
+        get_props_for_index = lambda index: [key for key in NORMALISATION[index].keys() if not key.startswith('_')]
         response = query(location)
-        _pd, _dhb, _cb, _szs = [
-            list(
-                filter(lambda hit: get_hits_from_index(hit, index), response))
+        pd, dhb, cb, szs, post, elec14 = [
+            get_es_props(
+                es_geo_index_result(
+                    list(
+                        filter(lambda hit: get_hits_from_index(hit, index),
+                               response)),
+                    NORMALISATION[index]['_allow_multiple']),
+                get_props_for_index(index))
             for index in [
                 'nz-police-district-boundaries',
                 'nz-district-health-boards-2012',
                 'nz-community-boards-2012-yearly-pattern',
-                'nz-school-zones-sept-2010'
+                'nz-school-zones-sept-2010',
+                'nz-post-postcode-boundaries-june-2011-licensed-only-for-mix-',
+                'general-electoral-district-boundaries-2014'
             ]
         ]
-        # TODO refactor, but allow for flexibility so different boundaries can
-        # implement different fields
-        pd = {
-            'name': _pd[0].properties[NORMALISATION[_pd[0].meta.index]['name']]
-            if _pd else None,
-            'id': _pd[0].properties[NORMALISATION[_pd[0].meta.index]['id']]
-            if _pd else None
-        }
-        dhb = {
-            'name':
-            _dhb[0].properties[NORMALISATION[_dhb[0].meta.index]['name']]
-            if _dhb else None,
-            'id': _dhb[0].properties[NORMALISATION[_dhb[0].meta.index]['id']]
-            if _dhb else None
-        }
-        cb = {
-            'name': _cb[0].properties[NORMALISATION[_cb[0].meta.index]['name']]
-            if _cb else None,
-            'id': _cb[0].properties[NORMALISATION[_cb[0].meta.index]['id']]
-            if _cb else None
-        }
-        szs = [{
-            'name': _sz.properties[NORMALISATION[_sz.meta.index]['name']],
-            'id': _sz.properties[NORMALISATION[_sz.meta.index]['id']],
-            'approvalDate':
-            _sz.properties[NORMALISATION[_sz.meta.index]['approvalDate']],
-            'effectiveDate':
-            _sz.properties[NORMALISATION[_sz.meta.index]['effectiveDate']],
-            'type': _sz.properties[NORMALISATION[_sz.meta.index]['type']],
-            'office': _sz.properties[NORMALISATION[_sz.meta.index]['office']],
-            'underReview':
-            _sz.properties[NORMALISATION[_sz.meta.index]['underReview']]
-        } for _sz in _szs]
 
         return CreateLocation(
             policeDistrict=PoliceDistrict(**pd),
             dhb=DHB(**dhb),
             communityBoard=CommunityBoard(**cb),
-            schoolZone=[SchoolZone(**sz) for sz in szs])
+            schoolZone=[SchoolZone(**sz) for sz in szs],
+            postCode=PostCode(**post),
+            electorate2014=Electorate2014(**elec14))
 
 
 class Mutations(graphene.ObjectType):
@@ -150,6 +145,18 @@ class SchoolZone(graphene.ObjectType):
         return self.underReview
 
 
+class PostCode(graphene.ObjectType):
+    postcode = graphene.String(description="Four-digit postcode")
+
+    def resolve_postcode(self, args, context, info):
+        return self.postcode
+
+
+class Electorate2014(graphene.ObjectType):
+    class Meta:
+        interfaces = (AdminBoundary, )
+
+
 class DHB(graphene.ObjectType):
     class Meta:
         interfaces = (AdminBoundary, )
@@ -167,6 +174,9 @@ class Query(graphene.ObjectType):
         CommunityBoard, description="NZ Community Board (2012)")
     dhb = graphene.Field(DHB, description="NZ District Health Board (2012)")
     schoolZone = graphene.Field(SchoolZone, description="NZ School Zone")
+    postCode = graphene.Field(PostCode, description="NZ Postal Code")
+    electorate2014 = graphene.Field(
+        Electorate2014, description="2014 NZ General Electorate")
 
 
 app = Flask(__name__)
